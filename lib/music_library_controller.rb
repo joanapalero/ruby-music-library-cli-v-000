d7d5bd565661ee9757c59ffab1909c13319a39d0
Joana Palero <joana.palero24@gmail.com>
2018-07-15 13:25:57 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,4 +41,46 @@ class MusicLibraryController
   
   def list_artists
     Artist.all.sort_by! {|artist| artist.name }.uniq.each_with_index do |artist, index|
-      puts "#"
+      puts "#{index +=1}. #{artist.name}"
+    end
+  end
+
+  def list_genres
+    Genre.all.sort_by! {|genre| genre.name }.uniq.each_with_index do |genre, index|
+      puts "#{index +=1}. #{genre.name}"
+    end
+  end
+
+  def list_songs
+    Song.all.sort_by! {|song| song.name }.uniq.each_with_index do |song, index|
+      puts "#{index +=1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist = gets.chomp
+    if artist = Artist.find_by_name(artist)
+      artist.songs.sort_by! {|song| song.name }.uniq.each_with_index do |song, index|
+        puts "#{index +=1}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre = gets.chomp
+    if genre = Genre.find_by_name(genre)
+      genre.songs.sort_by! {|song| song.name }.uniq.each_with_index do |song, index|
+        puts "#{index +=1}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    number = gets.chomp.to_i
+    song = Song.all.sort_by! {|song| song.name }[number -=1] if number > 0 && number < Song.all.count
+      puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+end
