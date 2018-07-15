a3f871f559864d306643af55b07dcc7db4ac9606
Joana Palero <joana.palero24@gmail.com>
2018-07-15 13:23:22 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -12,4 +12,18 @@ class MusicLibraryController
     
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
-      puts ""
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs by a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      
+      input = gets.strip.downcase
+      
+      case input
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres    
