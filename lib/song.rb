bccccec3ddc64f23e4ac9727db9f117542f48460
Joana Palero <joana.palero24@gmail.com>
2018-07-15 13:27:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,10 @@ class Song
   attr_accessor :name, :artist, :genre 
   @@all = []
   
-  def 
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  
