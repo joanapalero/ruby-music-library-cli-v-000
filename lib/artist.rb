dc40987c2c1ce70719604fa041f8199557f41d7b
Joana Palero <joana.palero24@gmail.com>
2018-07-15 12:59:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
   end
   
   def self.all
@@ -17,7 +16,7 @@ class Artist
   end
   
   def self.destroy_all
-    @@all.clear
+    @@all = []
   end
   
   def save
